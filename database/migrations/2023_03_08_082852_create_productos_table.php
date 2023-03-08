1615024334bcb6c104bf0feec992950056684e05
ezequiel <5582340@alu.murciaeduca.es>
2023-03-08 10:19:46 +0100

    creacion de migraciones

---
+++ database/migrations/2023_03_08_082852_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->decimal('precio')->nullable();
+            $table->string('categoria');
+            $table->string('imagen')->nullable();
+            $table->boolean('pendiente')->default(false);
+            $table->text('descripcion')->nullable();
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
